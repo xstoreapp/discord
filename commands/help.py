b6c674fc222ec4ced24617617894c4608a6a45cf
dragonDScript <agomila96@gmail.com>
2021-02-25 22:56:19 +0100

    Experimental msg system

---
+++ commands/help.py
@@ -0,0 +1,3 @@
+async def help(self, msg):
+    print("Hola")
+    await msg.channel.send("hello")
--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import os
 
 import discord
 
+import commands
+
 class Client(discord.Client):
   async def on_ready(self):
     print("Ready! Logged-in as {0}".format(self.user))
+  async def on_message(self, msg):
+    message = str.split(msg.content, " ")
+    if message[0] != "xstore":
+      return
+    selectedMsg = message[1]
+    commands[selectedMsg](self, msg)
 
 client = Client()
 token = os.getenv("TOKEN")
