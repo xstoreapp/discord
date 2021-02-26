a68a059d91037d07e302d9e9806b77c8a1d8551a
dragonDScript <agomila96@gmail.com>
2021-02-26 12:18:28 +0100

    Improve help embed

--- commands/help.py
+++ commands/help.py
@@ -1,3 +1,9 @@
+import discord
+
 async def help(self, msg):
-    print("Hola")
-    await msg.channel.send("hello")
+    embedMsg = discord.Embed(title="XStore Help", description="XStore's community first-party bot.", colour=0x000000)
+    embedMsg.add_field(name="help", value="This help page")
+    embedMsg.add_field(name="github",
+        value="This bot is open source. Source code available at [GitHub](https://github.com/xstoreapp/discord).")
+    embedMsg.set_footer(text="Made by dragonDScript")
+    await msg.channel.send(embed=embedMsg)
